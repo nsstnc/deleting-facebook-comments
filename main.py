4e9c221dac6cbb1428bf86957979d2ef7a46cad6
LEGION-LAPTOP\egorg <egorgolubev0484@gmail.com>
2024-06-20 13:51:15 +0300

    Updated proxies

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 import urllib.parse
-
+from config import ACCESS_TOKEN
 
 def extract_ids(url):
     # разбираем url поста на параметры
@@ -12,7 +12,7 @@ def extract_ids(url):
     # получаем id поста и id страницы
     story_fbid = query_params.get('story_fbid', [None])[0]
     page_id = query_params.get('id', [None])[0]
-
+    print(story_fbid, page_id)
     return story_fbid, page_id
 
 def get_new_token(cookies):
@@ -26,13 +26,19 @@ def delete_comment(token, comment_id, proxies=None):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    response = requests.delete(url, headers=headers, proxies=proxies)
+    response = requests.delete(url, headers=headers, proxies=proxies, timeout=30)
     return response.status_code, response.json()
 
 
-def clean_comments(post_id, tokens, proxies, cookies):
-    for token, cookie, proxy in zip(tokens, cookies, proxies):
-        comments = get_comments(post_id, token, proxy)
+def clean_comments(post_urls, tokens, proxies, cookies):
+    for post_url, token, cookie, proxy in zip(post_urls, tokens, cookies, proxies):
+        status_code, comments = get_comments(post_url, token, proxy)
+
+        if status_code == 401:
+            new_token = get_new_token(cookie)
+            status_code, comments = get_comments(post_url, new_token, proxy)
+
+
         for comment in comments:
             status_code, response = delete_comment(token, comment['id'], proxy)
             if status_code == 401:
@@ -48,22 +54,18 @@ def get_comments(post_url, token, proxies=None):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    response = requests.get(url, headers=headers, proxies=proxies)
-    if response.status_code == 200:
-        return response.json()['data']
-    else:
-        return []
+    response = requests.get(url, headers=headers, proxies=proxies, timeout=30)
+
+    return response.status_code, response.json()['data']
 
 
 
-post_url = 'https://www.facebook.com/permalink.php?story_fbid=pfbid032NzMt7pJsH7ek2cZ3FsXo5thnMi6Kyg6Qe2dZfYFpV9KN4RKAuc59xX447RfLg5Ml&id=61561075443726&rdid=AUAXGTL8VK7N1m87'
-tokens = ['EAAFfDEvgDWIBO8bFgxR8ZBBZCIZBZCu50ZC7Fe7mxe8Kei3fLaYwAfV6ZBgqgQEMnJLNHY4Bg9xlXd1tZAV8t9lTAv3XUfTgPxtGyVZB1q6ORBtDpkd0bCXzp3T5FxOx2IHosLItTFZCiQ0ZBD2K4Kh3rl1WKilqtZC1vrgPFSuVLFdHUcIrC69RMUNkfIf33tyZCgmrR8uB5KT6qjYInbqwqANHwyrh3QZDZD']
+post_urls = ['https://www.facebook.com/permalink.php?story_fbid=pfbid032NzMt7pJsH7ek2cZ3FsXo5thnMi6Kyg6Qe2dZfYFpV9KN4RKAuc59xX447RfLg5Ml&id=61561075443726&rdid=AUAXGTL8VK7N1m87']
+tokens = [ACCESS_TOKEN]
 proxies = [
-    {'http': 'http://164.163.42.27:10000',
-     'https': 'http://164.163.42.27:10000'},
-    {'http': 'http://181.198.53.6:3128',
-     'https': 'http://181.198.53.6:3128'},
+    {'http': 'http://103.153.154.6:80',
+     'https': 'http://199.167.236.12:3128'}
 ]
 cookies = ['']
 
-clean_comments(post_url, tokens, proxies, cookies)
+clean_comments(post_urls, tokens, proxies, cookies)
